ac7956ea6bc72d96f97da462ee471d49db6f14eb
Dan Brubaker Horst <dbrubak1@nd.edu>
2012-11-16 16:16:47 -0500

    Fetch SSIs; local to absolute paths

--- fetch_ssi.rb
+++ fetch_ssi.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby
 
-SOURCE="https://www.library.nd.edu/ssi/"
+DOMAIN="https://www.library.nd.edu/"
 
 includes = [
  'css',
@@ -11,13 +11,14 @@ includes = [
  'footer'
 ]
 
+# Cache the files locally
 includes.each do |include|
-  `wget #{SOURCE}#{include}.shtml -O ssi/#{include}.shtml --user-agent="SUSPICIOUS SCRIPT"`
+  `wget #{SOURCE}ssi/#{include}.shtml -O ssi/#{include}.shtml --user-agent="SUSPICIOUS SCRIPT"`
 end
 
-#https://www.library.nd.edu/ssi/css.shtml
-#https://www.library.nd.edu/ssi/js.shtml
-#https://www.library.nd.edu/ssi/skip-links.shtml
-#https://www.library.nd.edu/ssi/header.shtml
-#https://www.library.nd.edu/ssi/main-menu.shtml
-#https://www.library.nd.edu/ssi/footer.shtml
+# Convert relative paths to absolute paths
+includes.each do |include|
+  # href="/
+  # src="/
+  # DOMAIN
+end
